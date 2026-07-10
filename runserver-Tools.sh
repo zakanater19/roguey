974dbf45b71a028c46908d894700aaacc3bf47bf
randomname <tasteliketoast1@gmail.com>
2026-07-10 18:22:07 +0100

    map

--- runserver-Tools.sh
+++ runserver-Tools.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Server --configuration Tools
+dotnet run --project Content.Server --configuration Tools -- --data-dir Resources
 read -p "Press enter to continue"
--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Server
+dotnet run --project Content.Server -- --data-dir Resources
 read -p "Press enter to continue"
